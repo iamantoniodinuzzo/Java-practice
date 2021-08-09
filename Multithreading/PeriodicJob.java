e9abe43c18b6d987eb23736c2523d000c615e47b
Antonio <tonydinu02@gmail.com>
2021-08-09 10:16:32 +0200

    Update PeriodicJob.java

--- Multithreading/PeriodicJob.java
+++ Multithreading/PeriodicJob.java
@@ -1,9 +1,27 @@
-public static void  periodicJob(Runnable r, int p){
-	new Thread(){
-		try{
-			currentThread().sleep(p);
-		}catch(InterruptedException ie){
-			r.run();
-		}
+public class PeriodicJob{
+	public static void main(String[] args){
+		Runnable r = new Runnable(){
+			public void run(){
+				System.out.println("ciao");
+			}
+		};
+		periodicJob(r,1000);		
+	}
+
+	public static void  periodicJob(Runnable r, int p){
+		new Thread(){
+			@Override
+			public void run(){
+				while(!Thread.currentThread().isInterrupted()){
+					new Thread(r).start();
+					try{
+						Thread.sleep(p);
+					}catch(InterruptedException e){
+						return;
+					}	
+				}
+			}
+		};
+		myThread.start();
 	}
 }
