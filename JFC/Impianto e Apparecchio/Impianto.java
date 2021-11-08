d20218af47422d3b49a763843d05172eb19db47e
Antonio <tonydinu02@gmail.com>
2021-11-08 11:21:02 +0100

    Rimozione Main per collisione di esecuzione

--- JFC/Impianto e Apparecchio/Impianto.java
+++ JFC/Impianto e Apparecchio/Impianto.java
@@ -28,5 +28,18 @@ public class Impianto {
         return potenzaAssorbitaApparecchi;
     }
 
+    public static void main(String[] args) {
+        Apparecchio tv = new Apparecchio(150);
+        Apparecchio radio = new Apparecchio(30);
+        Impianto i = new Impianto(3000);
+        i . collega (tv) ;
+        i . collega (radio) ;
+        System.out.println( i .potenza());
+        tv.on();
+        System.out.println( i .potenza());
+        radio.on();
+        System.out.println( i .potenza());
+    }
+
 
 }
