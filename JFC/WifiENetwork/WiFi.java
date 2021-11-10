2199735e12749394e81e4f1503f0d7119b3fc5f5
Antonio <tonydinu02@gmail.com>
2021-11-10 08:51:58 +0100

    Update WiFi.java

--- JFC/WifiENetwork/WiFi.java
+++ JFC/WifiENetwork/WiFi.java
@@ -4,10 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashSet;
 import java.util.Iterator;
-import java.util.TreeSet;
 
 public class WiFi implements Iterable<WiFi.Network> {
     private ArrayList<Network> networks;
